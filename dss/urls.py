5b83bb1d2cc5294df74b7b49eba6c606ea65b328
Anis Bouaziz <mohamedanis.bouaziz@esprit.tn>
2021-12-24 13:06:23 +0100

    dash

--- dss/urls.py
+++ dss/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from tool import views
 urlpatterns = [
     path('', include('tool.urls')),
     path('admin/', admin.site.urls),
