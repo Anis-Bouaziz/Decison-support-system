aae8bd940cd32fe31ed2a80ca11308e3db225b46
Anis Bouaziz <mohamedanis.bouaziz@esprit.tn>
2021-12-24 17:50:51 +0100

    plots

--- tool/views.py
+++ tool/views.py
@@ -1,35 +1,24 @@
 from django.shortcuts import render
-from django_plotly_dash import DjangoDash
-from dash import dash_table
-import dash_bootstrap_components as dbc
-from tool.methods.WSM import WSM
-from tool.methods.Entropy import entropy
-from tool.methods.promethee import promethee
-from tool.methods.topsis import topsis
-import pandas as pd
-import plotly.express as px
-from tool.methods.waspass import waspass
-from dash import html
-from dash import dcc
 
+import pandas as pd
+from tool.dash import top_wsm ,top_topsis,top_ent,top_prom,top_was
 
 df = pd.read_csv('tool/data/data.csv')
-topassets = df.groupby(by=['Company']).sum().sort_values(
-    by='Assets ($billion)', ascending=False).head(10)['Assets ($billion)'].to_dict()
-topmarket = df.groupby(by=['Company']).sum().sort_values(
-    by='Market Value ($billion)', ascending=False).head(10)['Market Value ($billion)'].to_dict()
-topprofits = df.groupby(by=['Company']).sum().sort_values(
-    by='Profits ($billion)', ascending=False).head(10)['Profits ($billion)'].to_dict()
-topCountries = df.groupby(by=['Country']).sum().sort_values(
-    by='Market Value ($billion)', ascending=False).head(10)['Market Value ($billion)']
-topsales = df.groupby(by=['Company']).sum().sort_values(
-    by='Sales ($billion)', ascending=False).head(10)['Sales ($billion)'].to_dict()
-df.drop(['Global Rank', 'Continent', 'Latitude',
-        'Longitude', 'Country'], axis=1, inplace=True)
+
+
 
 
 def index(request):
-
+    topassets = df.groupby(by=['Company']).sum().sort_values(
+    by='Assets ($billion)', ascending=False).head(10)['Assets ($billion)'].to_dict()
+    topmarket = df.groupby(by=['Company']).sum().sort_values(
+    by='Market Value ($billion)', ascending=False).head(10)['Market Value ($billion)'].to_dict()
+    topprofits = df.groupby(by=['Company']).sum().sort_values(
+    by='Profits ($billion)', ascending=False).head(10)['Profits ($billion)'].to_dict()
+    topCountries = df.groupby(by=['Country']).sum().sort_values(
+    by='Market Value ($billion)', ascending=False).head(10)['Market Value ($billion)']
+    topsales = df.groupby(by=['Company']).sum().sort_values(
+    by='Sales ($billion)', ascending=False).head(10)['Sales ($billion)'].to_dict()
     return render(request, 'tool/index.html', {'topCountries': topCountries.to_dict(), 'topassets': topassets, 'topprofits': topprofits, 'topmarket': topmarket, 'topsales': topsales})
 
 
@@ -39,39 +28,25 @@ def charts(request):
 
 
 def WS(request):
-    w, wn = WSM.wsm(df)
-
-    top1 = df[df['Company'] == w[:1]['Company'].values[0]]
-
-    return render(request, 'tool/WSM.html', {'top1': top1})
+   
+    return render(request, 'tool/WSM.html', {'top1': top_wsm})
 
 
 def entropyMethod(request):
-    w, wnorm, w_step3, e1, e2, e3 = entropy(df)
-
-    top1 = df[df['Company'] == w[:1]['Company'].values[0]]
-
-    return render(request, 'tool/entropy.html', {'top1': top1})
+    
+    return render(request, 'tool/entropy.html', {'top1': top_ent})
 
 
 def topsisMethod(request):
 
-    w, wnorm, step2, step3, step4 = topsis(df)
-    top1 = df[df['Company'] == w[:1]['Company'].values[0]]
-
-    return render(request, 'tool/topsis.html', {'top1': top1})
+    return render(request, 'tool/topsis.html', {'top1': top_topsis})
 
 
 def waspassMethod(request):
 
-    w, wnorm = waspass(df)
-    top1 = df[df['Company'] == w[:1]['Company'].values[0]]
-
-    return render(request, 'tool/waspass.html', {'top1': top1})
+    return render(request, 'tool/waspass.html', {'top1': top_was})
 
 
 def prometheeMethod(request):
-    w, wnorm, phi = promethee(df)
-    top1 = df[df['Company'] == w[:1]['Company'].values[0]]
-    #############################################################
-    return render(request, 'tool/promethee.html', {'top1': top1})
+ 
+    return render(request, 'tool/promethee.html', {'top1': top_prom})
